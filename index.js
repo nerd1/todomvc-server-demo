50c7a6a48855e000c6f18838c869eec8531e518e
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-05-10 20:05:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import express from 'express'
+let app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3100)
+
